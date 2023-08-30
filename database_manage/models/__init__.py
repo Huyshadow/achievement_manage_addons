f793298acb3fdb7545dba73b303e9e49f1d14ec2
Huy Dang <huy.dang110902@gmail.com>
2023-08-30 19:57:05 +0700

    complete skeleton

--- database_manage/models/__init__.py
+++ database_manage/models/__init__.py
@@ -6,6 +6,6 @@ from . import result
 from . import contact_info
 from . import closure_criteria
 from . import user
-# from . import criteria
+from . import criteria
 from . import result_of_criteria
 from . import submission
--- database_manage/models/criteria.py
+++ database_manage/models/criteria.py
@@ -8,34 +8,34 @@ class Criteria(models.Model):
     name = fields.Char(required=True)
     isCriteria = fields.Boolean(required=True, default=False) 
     method = fields.Char(required=True, default='binary')
-    note = fields.Char(required=True,default='')
-    valueListString = fields.Char(required=True,default='')
-    content = fields.Char(required=True,default='')
+    note = fields.Char(required=True, default='')
+    valueListString = fields.Char(required=True, default='')
+    content = fields.Char(required=True, default='')
     type = fields.Selection([
         ('hard','Hard'),
         ('other_type','Other_type')
     ], default = 'hard', required=True)
     evidence = fields.Boolean(required=True, default=False) 
     lowerSign = fields.Selection([
-        ('<'),
-        ('<='),
-        ('>'),
-        ('>=')
-    ], default=('>'))
+        ('<', '<'),
+        ('<=', '<='),
+        ('>', '>'),
+        ('>=', '>=')
+    ], default=('>'),required=True)
     upperSign = fields.Selection([
-        ('<'),
-        ('<='),
-        ('>'),
-        ('>=')
-    ], default=('>'))
-    point = fields.Float(required=True, default=0)
+        ('<', '<'),
+        ('<=', '<='),
+        ('>', '>'),
+        ('>=', '>=')
+    ], default=('>'), required=True)
+    point = fields.Float(default=0,required=True)
     lowerPoint = fields.Float(default=0, required=True)
     upperPoint = fields.Float(default=0, required=True)
     createAt = fields.Datetime(default=fields.Datetime.now, required=True)
     updateAt = fields.Datetime(default=fields.Datetime.now, required=True)
     deleteAt= fields.Datetime()
     achievementId = fields.Integer() #Will be defined 
-    
+
     sql_constraints = [
         ('criteria_pk', 'PRIMARY KEY (id)', '''Criteria's ID must be Primary Key'''),
     ]
