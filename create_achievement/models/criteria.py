55491de3f26c2ae77c693b72b28955c4ecbca003
Huy Dang <huy.dang110902@gmail.com>
2023-09-07 03:23:15 +0700

    Function are done

--- create_achievement/models/criteria.py
+++ create_achievement/models/criteria.py
@@ -6,40 +6,35 @@ class Criteria(models.Model):
     _description = ' Criteria for Tuyen duong Website'
 
     parent_id = fields.Many2one(
-        'create_achievement.achievement', string="Criterias for Achievement", required=True)
+        'create_achievement.achievement', string="Criterias for Achievement")
 
     id = fields.Integer(default=lambda self: self.env['ir.sequence'].next_by_code(
-        'create.achievement.criteria'), required=True, copy=True, readonly=True)
+        'create.achievement.criteria'), copy=True, readonly=True)
     type = fields.Selection([
         ('tiêu chí', 'Tiêu chí'),
         ('tập tiêu chí', 'Tập tiêu chí')
-    ], default='tiêu chí', required=True)
+    ], default='tiêu chí')
     name = fields.Char(required=True)
-    is_criteria = fields.Boolean(required=True, default=False)
+    is_criteria = fields.Boolean(default=False)
     method = fields.Selection(
         [('thang điểm', 'Thang điểm'), ('nhi phân', 'Nhị Phân'), ('người nộp tự nhận xét', 'Người nộp tự nhận xét'), ('dạng danh sách', 'Dạng danh sách')], required=True, default='Thang đi')
-
-    value_list_string = fields.Char(required=True, default='')
+    value_list_string = fields.Char(default='')
     note = fields.Char(required=True, default='')
     content = fields.Char(required=True, default='')
     evidence = fields.Boolean(required=True, default=False)
-    left_goal = fields.Integer(default=0)
-    right_goal = fields.Integer(default=0)
     sign = fields.Selection([
-        ('a', '<'),
+        ('<', '<'),
         ('<=', '<='),
-        ('b', '>'),
+        ('>', '>'),
         ('>=', '>='),
         ('''>= và <=''', '''>= và <='''),
         ('''>= hoặc <=''', '''>= hoặc <='''),
     ], default=('>'), required=True)
-    upper_sign = fields.Selection([
-        ('<', '<'),
-        ('<=', '<='),
-        ('>', '>'),
-        ('>=', '>=')
-    ], default=('>'), required=True)
     point = fields.Float(default=0, required=True)
-    lowerPoint = fields.Float(default=0, required=True)
-    upperPoint = fields.Float(default=0, required=True)
+    lower_point = fields.Float(default=0, required=True)
+    upper_point = fields.Float(default=0, required=True)
     deleteAt = fields.Datetime()
+
+    """ group_id = fields.Many2one(
+        'create_achievement.group_criteria', string="Criterias of Group Criteria"
+    ) """
