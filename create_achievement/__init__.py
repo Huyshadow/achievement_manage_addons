f5b03d3883797224c0454ffac4a0efb0ddf94e9b
Huy Dang <huy.dang110902@gmail.com>
2023-09-09 20:13:44 +0700

    Success

--- create_achievement/__init__.py
+++ create_achievement/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from . import controllers
-<<<<<<< HEAD
 from . import models
 from . import views
-=======
-from . import models
->>>>>>> 62ccb99 (Skeleton of Create Achievement)
--- create_achievement/__manifest__.py
+++ create_achievement/__manifest__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 {
-<<<<<<< HEAD
     'name': "Create Achievement",
 
     'summary': """
@@ -13,20 +12,6 @@
 
     'author': "My Company",
     'website': "https://www.facebook.com/profile.php?id=100074736126982",
-=======
-    'name': "create_achievement",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
->>>>>>> 62ccb99 (Skeleton of Create Achievement)
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -39,7 +24,6 @@
 
     # always loaded
     'data': [
-<<<<<<< HEAD
         'security/ir.model.access.csv',
         'data/ir_cron.xml',
         'data/achievement.xml',
@@ -48,11 +32,6 @@
         'views/menu.xml',
         'views/criteria.xml'
 
-=======
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
->>>>>>> 62ccb99 (Skeleton of Create Achievement)
     ],
     # only loaded in demonstration mode
     'demo': [
