e2e9744ad3b8947d8b4d19c762fae08a4f0bb95a
Huy Dang <huy.dang110902@gmail.com>
2023-09-09 19:37:08 +0700

    check

--- create_achievement/models/achievement.py
+++ create_achievement/models/achievement.py
@@ -1,17 +1,18 @@
 from odoo import models, fields, api
 
+
 class Achievement(models.Model):
     _name = 'create_achievement.achievement'
     _description = 'Achievement Model of Create-Achievement Module'
 
-    _parent_store=True
+    parent_id = fields.Many2one(
+        'create_achievement.achievement', string="Root Model")
 
-
-    id = fields.Integer(string="ID", default=lambda self: self.env['ir.sequence'].next_by_code('create.achievement.achievement'))
-    name = fields.Char(string="Name",required=True)
-    softCriteria = fields.Integer(string="Soft Criteria",required=True)
-    description = fields.Text()
-    updateAt = fields.Datetime()
+    id = fields.Integer(string="ID", default=lambda self: self.env['ir.sequence'].next_by_code(
+        'create.achievement.achievement'))
+    name = fields.Char(string="Name", required=True)
+    softCriteria = fields.Integer(string="Soft Criteria", required=True)
+    description = fields.Text(string="Description")
     endAt = fields.Datetime()
     startAt = fields.Datetime()
     end_submit_at = fields.Datetime()
@@ -24,8 +25,7 @@ class Achievement(models.Model):
         ('other_type', 'Other_type')
     ], default='achievement', required=True)
     manageUnit = fields.Text(default='{}')
-    deleteAt=fields.Datetime()
+    deleteAt = fields.Datetime()
     _sql_constraints = [
         ('achievement_pk', 'PRIMARY KEY (id)', 'Achievement ID must be unique.'),
     ]
-
