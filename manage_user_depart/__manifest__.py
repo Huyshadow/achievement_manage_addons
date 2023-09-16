953df9778beb723d91870bb717de528c39513f41
Nguyen Phuc Binh Khang <91519610+khangnpb@users.noreply.github.com>
2023-09-16 12:12:39 +0700

    finish quản lý dơn vị

---
+++ manage_user_depart/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quản lý đơn vị",
+
+    'summary': """
+        Module cho phép xem danh sách các user và các đơn vị""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Themes/Backend",
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','access_right_user'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/Department_Manage_View/department_view.xml',
+        'views/User_Manage_View/user_view.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'manage_user_depart/static/src/scss/heightbox.scss'
+        ],
+        # 'web.assets_frontend': [
+        #     'manage_user_depart/static/src/scss/heightbox.scss'
+        # ],
+    },
+   
+}
