cb46e910d033f07bcabd12dccf9be448781cb0bd
tus <tus@LAPTOP-NPCU9BU7>
2023-09-17 10:58:17 +0700

    demo for user profile

---
+++ user_view_profile/models/user.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+class User(models.Model):
+    _inherit = 'res.users'
+    
+    @api.model
+    def open_user_profile(self):
+        return {
+            'name': 'Thông tin cá nhân',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'views': [(False, 'form')],
+            'res_model': 'res.users',
+            'res_id': self.env.user.id,
+            'target': 'current',
+        }
