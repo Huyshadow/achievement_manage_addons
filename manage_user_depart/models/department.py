40ed1084794d11fe3a6b825b34fcea8eff9ce375
Nguyen Phuc Binh Khang <91519610+khangnpb@users.noreply.github.com>
2023-09-19 21:16:49 +0700

    update department

--- manage_user_depart/models/department.py
+++ manage_user_depart/models/department.py
@@ -10,6 +10,12 @@ class Department(models.Model):
     #     'database.manage.department'), required=True, copy=True, readonly=True)
     code = fields.Char()
     name = fields.Char(string="Name")
+    user_count = fields.Integer(string="Số lượng người dùng", compute="_compute_user_count")
+
+    # @api.depends('user_ids')
+    def _compute_user_count(self):
+        for department in self:
+            department.user_count = self.env['res.users'].search_count([('donvi', '=', department.id)])
     # updateAt = fields.Datetime()
     # deleteAt = fields.Datetime()
 
