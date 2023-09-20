2f777df3b191392f5f39bb2358be4d9b2f5a0b97
Nguyen Phuc Binh Khang <91519610+khangnpb@users.noreply.github.com>
2023-09-20 22:16:08 +0700

    reduce code

--- manage_user_depart/models/department.py
+++ manage_user_depart/models/department.py
@@ -6,23 +6,11 @@ class Department(models.Model):
     _name = 'manage_user_depart.department'
     _description = 'Department of Tuyen duong Website'
 
-    # departmentId = fields.Integer(default=lambda self: self.env['ir.sequence'].next_by_code(
-    #     'database.manage.department'), required=True, copy=True, readonly=True)
     code = fields.Char()
     name = fields.Char(string="Name")
     user_count = fields.Integer(string="Số lượng người dùng", compute="_compute_user_count")
 
-    # @api.depends('user_ids')
     def _compute_user_count(self):
         for department in self:
             department.user_count = self.env['res.users'].search_count([('donvi', '=', department.id)])
-    # updateAt = fields.Datetime()
-    # deleteAt = fields.Datetime()
-
-    # _sql_constraints = [
-    #     ('department_unique_name', 'UNIQUE (name)',
-    #      'Department Name must be unique'),
-    #     ('department_unique_code', 'UNIQUE (code)',
-    #      'Department Code must be unique'),
-    # ]
 
