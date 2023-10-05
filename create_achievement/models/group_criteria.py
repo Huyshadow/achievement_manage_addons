c41c2496e666bbc03b2e6b79288e0b87f5c029f4
Huy Dang <huy.dang110902@gmail.com>
2023-10-06 02:01:16 +0700

    Check

--- create_achievement/models/group_criteria.py
+++ create_achievement/models/group_criteria.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from lxml import etree
 
 
 class GroupCriterias(models.Model):
@@ -9,12 +10,50 @@ class GroupCriterias(models.Model):
         "create_achievement.achievement", string="Danh hiệu")
 
     name = fields.Char(string="Tên tập tiêu chí", required=True)
+    num_of_group = fields.Integer(
+        string="Số tập tiêu chí", required=True, default=0)
 
-    constrait_criterias = fields.One2many(
-        "create_achievement.criteria", "parent_id_constraint", string="Danh sách tiêu chí bắt buộc")
+    # --------------------------------------------------------
+    option_1_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_1")
+    option_1_label = fields.Char(default="", translate=True)
 
-    option_criterias = fields.One2many(
-        "create_achievement.criteria", "parent_id_option", string="Danh sách tiêu chí khác")
+    @api.depends('option_1_label')
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        # for record in self:
+        result = super(GroupCriterias, self).fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        print(result)
+        if view_type == 'form':
+            doc = etree.XML(result['arch'])
+            option_name = doc.xpath(
+                "//page[@name='option_1']")
+            if option_name:
+                option_name[0].set("string", 'Huy')
+                option_name[0].addnext(etree.Element(
+                    'label', {'string': 'Huy'}))
+                result['arch'] = etree.tostring(doc, encoding='unicode')
+        print(result)
+        return result
+    # -------------------------------------------------------
+    option_2_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_2")
+    option_3_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_3")
+    option_4_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_4")
+    option_5_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_5")
+    option_6_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_6")
+    option_7_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_7")
+    option_8_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_8")
+    option_9_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_9")
+    option_10_criterias = fields.One2many(
+        "create_achievement.criteria", "parent_id_option_10")
 
     option_constraint = fields.Integer(
         string="Số tiêu chí khác bắt buộc", default=1)
@@ -22,3 +61,10 @@ class GroupCriterias(models.Model):
         string="Mô tả", default="Không"
     )
     display_name = fields.Char(string="Tên hiển thị", store=True)
+
+    @api.constrains('num_of_group')
+    def _check_num_of_group(self):
+        for record in self:
+            if record.num_of_group > 10:
+                raise ValidationError(
+                    "Không thể tạo quá 10 tập tiêu chí")
