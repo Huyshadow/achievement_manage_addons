f1338cf1df5967fcabe0a9fc7b337d07ac17b737
tus <tus@LAPTOP-NPCU9BU7>
2023-10-07 13:35:51 +0700

    fix_view_achievement

--- create_achievement/models/group_criteria.py
+++ create_achievement/models/group_criteria.py
@@ -7,7 +7,7 @@ class GroupCriterias(models.Model):
 
     parent_id = fields.Many2one(
         "create_achievement.achievement", string="Danh hiệu")
-    criteria_ids = fields.One2many(
+    type_criteria_ids = fields.One2many(
         'create_achievement.type_criterias', 'parent_id', string="Danh sách dạng tiêu chí")
 
     name = fields.Char(string="Tên tập tiêu chí", required=True)
