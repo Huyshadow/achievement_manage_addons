66cef018fb345f1522c60ab6c674510222c860c5
Huy Dang <huy.dang110902@gmail.com>
2023-10-07 15:21:43 +0700

    Fix something

--- create_achievement/models/type_criteria.py
+++ create_achievement/models/type_criteria.py
@@ -7,7 +7,8 @@ class TypeCriterias(models.Model):
 
     parent_id = fields.Many2one(
         "create_achievement.group_criterias", string="Dạng tiêu chí")
-    criteria_ids = fields.One2many('create_achievement.criteria', 'parent_id', string="Danh sách tiêu chí")
+    criteria_ids = fields.One2many(
+        'create_achievement.criteria', 'parent_id', string="Danh sách tiêu chí")
     name = fields.Char(string="Tên Dạng tiêu chí", required=True)
-    constraint = fields.Boolean(string="Bắt buộc?", default=True)
-    nums_of_option = fields.Integer(default=0)
+    constraint = fields.Boolean(string="Bắt buộc", default=True)
+    nums_of_option = fields.Integer(string="Số tiêu chí phải thỏa", default=0)
