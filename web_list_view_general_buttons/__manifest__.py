ebed50407e2fc825dc59dda3b0b33900b7452fda
Huy Dang <huy.dang110902@gmail.com>
2023-10-17 12:16:06 +0700

    Delete Unnecessary file

--- web_list_view_general_buttons/__manifest__.py
+++ web_list_view_general_buttons/__manifest__.py
@@ -1,20 +1,4 @@
-# ©  2018 Terrabit
-# See README.rst file on addons root folder for license details
-
-# {
-#     "name": "List View General Buttons",
-#     "summary": "General Buttons in List View ",
-#     "version": "13.0.1.0.0",
-#     "author": "Terrabit, Odoo Community Association (OCA)",
-#     "website": "https://www.terrabit.ro",
-#     "category": "Generic Modules",
-#     "depends": ["base", "web"],
-#     "data": ["views/assets.xml"],
-#     "qweb": ["static/src/xml/*.xml"],
-#     "development_status": "Mature",
-#     "maintainers": ["dhongu"],
-# }
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
     'name': "List View General Buttons",
 
