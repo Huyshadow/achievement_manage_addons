7e09616cd90bf2352fe6243f7a0cd0e38bcee8e8
Huy Dang <huy.dang110902@gmail.com>
2023-10-20 10:11:45 +0700

    Checking

--- graph_view_achievement/models/statistic_depart.py
+++ graph_view_achievement/models/statistic_depart.py
@@ -7,7 +7,21 @@ class Statistic_Depart(models.Model):
 
     achievement_id = fields.Many2one(
         'create_achievement.achievement', string="ID danh hiệu")
-    num_of_submit = fields.Char()
-    num_of_accept = fields.Char()
+    num_of_submit = fields.Char(default="0")
+    submit_nums = fields.Integer(compute="_compute_submit", store=True)
+    num_of_accept = fields.Char(default="0")
+    accept_nums = fields.Integer(compute="_compute_accept", store=True)
     depart_name = fields.Char()
     depart_id = fields.Char()
+
+    @api.depends('num_of_submit')
+    def _compute_submit(self):
+        for record in self:
+            record.submit_nums = int(record.num_of_submit)
+            print(type(record.submit_nums))
+
+    @api.depends('num_of_accept')
+    def _compute_accept(self):
+        for record in self:
+            record.accept_nums = int(record.num_of_accept)
+            print(type(record.accept_nums))
